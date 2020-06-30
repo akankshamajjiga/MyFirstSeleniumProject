64d2e91f6e0d92b29a6940ffed6871c9866d3b1c
akankshamajjiga <akankshamajjiga@gmail.com>
2020-06-30 21:06:12 +0530

    added new testcase day3

--- Readme.md
+++ Readme.md
@@ -1,1 +1,10 @@
-In this project, I have learnt the basics of selenium by automating the scenario of gmail login
+In this project, I have learnt the basics of selenium by automating the scenario of gmail login.
+
+in this project,i have learnt about a selector i.e css selector to find the element's of email and password field element's to pass the sendkeys to enter the email 
+and password which i want to give and also to find the next button element to click on it.
+and how to maximize (or) minimize the browser window.
+and wait until condition using different elements like elementtobeclicked,elementtobeselected etc. 
+and click and quit driver's to close or to exit the browser.and also explicit wait condition to wait for timeout.
+and also how to use setproperty by telling the webdriver to know the path of the chrome driver to open my browser.
+we can instantiate the web driver to different web browser's like chrome,firefox etc.
+i have learnt about how to get the url of my current web browser.
---
+++ src/AjioLogin.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AjioLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\softwares\\chromedriver_win32\\chromedriver.exe");
+		
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("profile.default_content_setting_values.notifications", 2);
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", prefs);
+		
+		WebDriver driver = new ChromeDriver(options);
+		
+		driver.get("https://www.ajio.com/");
+	
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.cssSelector(".login-form.login-modal")).click();
+		
+		driver.findElement(By.cssSelector(".username")).sendKeys("learnerChachi@gmail.com");
+		
+		driver.findElement(By.cssSelector(".login-btn")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver,20);
+	    
+	    wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".login-form-inputs.hide-pwd-otp")));
+	    
+		driver.findElement(By.cssSelector(".login-form-inputs.hide-pwd-otp")).sendKeys("Aku@1997");
+		
+		driver.findElement(By.cssSelector(".login-form-inputs.login-btn")).click();
+	
+		//wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.linkText("Sign Out"))));
+		
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+		driver.findElement(By.linkText("Sign Out")).click();
+	
+   }
+}
